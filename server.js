04d4488091baf900e9327008647cf2d50bdafe82
marcial199545 <marcial.jimenez.caballero@gmail.com>
2019-03-29 11:24:53 -0700

    setup start script and heroku

--- server.js
+++ server.js
@@ -53,6 +53,11 @@ app.get("/about", (req, res) => {
                 pageTitle: "About page"
         });
 });
+app.get("/projects", (req, res) => {
+        res.render("projects.hbs", {
+                pageTitle: "Projects"
+        });
+});
 app.get("*", (req, res) => {
         //NOTE seding an error object if page is not found
         res.send({
